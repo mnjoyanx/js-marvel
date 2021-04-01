834592e5614d18995ea46634b366d15734f68ccd
a-eliza <eliza.aleksanyan94@gmail.com>
2021-04-01 18:22:49 +0400

    class for ajax request

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log('hello world')
+import 'regenerator-runtime/runtime'
+import axios from 'axios'
+
+const API_KEY = 'a5837db97d72016c81a7a776f4240db9'
+const URL = 'https://gateway.marvel.com/v1/public/comics'
+
+class Datas {
+    async getAllData(url) {
+        const res = await axios.get(url, {
+            params: {
+                apikey: API_KEY
+            }
+        })
+
+        console.log(res.data.result)
+    }
+}
+
+const data = new Datas()
+data.getAllData(URL)
