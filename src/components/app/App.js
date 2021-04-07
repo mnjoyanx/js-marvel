8ad6299e0187bd688df0c6d0bede22cb0b45b03e
mnjoyanx <tigranmnjoyan@gmail.com>
2021-04-07 22:58:54 +0400

    scrollbar

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,8 +1,10 @@
 import {URL, URL_COMICS, URL_CHARACTERS} from '../../constants/api'
 import {getData} from '../../utils/getAllData'
+import '/index.css'
 
 class App {
     async render() {
+        document.body.append('hello')
         const result = await getData.getAllData(URL + URL_CHARACTERS)
         return result
     }
